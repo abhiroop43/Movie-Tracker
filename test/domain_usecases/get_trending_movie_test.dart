7eedc6c21bc402f5c1d3938d5ec8b78d4dd4c6f3
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-08-14 00:55:19 +0400

    feat(movie): :sparkles: refine Movie entity and introduce GetTrendingMovies use case
    * Removed unnecessary properties from Movie entity for clarity and simplicity
    * Established GetTrendingMovies use case to fetch trending movies from the repository
    * Added tests to ensure the correctness of the GetTrendingMovies functionality

---
+++ test/domain_usecases/get_trending_movie_test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:movie_tracker/domain/entities/movie.dart';
+import 'package:movie_tracker/domain/repositories/movie_repository.dart';
+import 'package:movie_tracker/domain/usecases/get_trending_movies.dart';
+
+import 'get_trending_movie_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<MovieRepository>()])
+void main() {
+  late GetTrendingMovies usecase;
+  late MockMovieRepository mockMovieRepository;
+
+  setUp(() {
+    mockMovieRepository = MockMovieRepository();
+    usecase = GetTrendingMovies(mockMovieRepository);
+  });
+
+  final tMovieList = [
+    Movie(
+      id: 1,
+      title: "Test Movie 1",
+      overview: "Desc 1",
+      posterPath: "/image1",
+    ),
+    Movie(
+      id: 2,
+      title: "Test Movie 2",
+      overview: "Desc 2",
+      posterPath: "/image2",
+    ),
+  ];
+
+  test("should get trending movies from the repository", () async {
+    // arrange
+    when(
+      mockMovieRepository.getTrendingMovies(),
+    ).thenAnswer((_) async => tMovieList);
+
+    // act
+    final result = await usecase();
+
+    // assert
+    expect(result, tMovieList);
+
+    verify(mockMovieRepository.getTrendingMovies());
+    verifyNoMoreInteractions(mockMovieRepository);
+  });
+}
